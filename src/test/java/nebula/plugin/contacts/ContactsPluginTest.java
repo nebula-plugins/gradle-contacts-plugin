ad53d8b41198af771f18e487fde73f31c1e48540
John Burns <wakingrufus@gmail.com>
2025-10-28 14:59:19 -0500

    add test to verify multiproject behavior
    remove redundant gradle wrapper validation action (setup-gradle takes care of this now)

--- src/test/java/nebula/plugin/contacts/ContactsPluginTest.java
+++ src/test/java/nebula/plugin/contacts/ContactsPluginTest.java
@@ -44,6 +44,66 @@ class ContactsPluginTest {
 
     @ParameterizedTest
     @EnumSource(SupportedGradleVersion.class)
+    void test_multiproject(SupportedGradleVersion gradleVersion) {
+        final TestProjectBuilder builder = TestProjectBuilder.testProject(projectDir);
+        builder.rootProject().plugins().id("com.netflix.nebula.contacts");
+        builder.rootProject()
+                .rawBuildScript(
+                        //language=kotlin
+                        """
+                                contacts {
+                                    addPerson("example@example.com") {
+                                        moniker = "Nebula-Plugins maintainers"
+                                        github = "nebula-plugins"
+                                        role("owner")
+                                    }
+                                }
+                                """);
+        final var subProject1 = builder.subProject("sub1");
+        subProject1.plugins().id("com.netflix.nebula.contacts");
+        subProject1.rawBuildScript(
+                //language=kotlin
+                """
+                        afterEvaluate {
+                            plugins.withType<nebula.plugin.contacts.BaseContactsPlugin>() {
+                                println("sub1 contacts: " + getAllContacts().size)
+                            }
+                        }
+                        """);
+
+        final var subProject2 = builder.subProject("sub2");
+        subProject2.plugins().id("com.netflix.nebula.contacts");
+        subProject2.rawBuildScript(
+                //language=kotlin
+                """
+                        contacts {
+                            addPerson("example2@example.com") {
+                                moniker = "Nebula-Plugins maintainers"
+                                github = "nebula-plugins"
+                                role("owner")
+                            }
+                        }
+                        afterEvaluate {
+                            plugins.withType<nebula.plugin.contacts.BaseContactsPlugin>() {
+                                println("sub2 contacts: " + getAllContacts().size)
+                            }
+                        }
+                        """);
+        final var runner = builder.build(BuildscriptLanguage.KOTLIN);
+
+        BuildResult result = runner.run(
+                GradleRunner.create().forwardOutput().withGradleVersion(gradleVersion.version),
+                "build");
+        assertThat(result)
+                .hasNoDeprecationWarnings()
+                .hasNoMutableStateWarnings();
+        assertThat(result.getOutput())
+                .contains("sub1 contacts: 1")
+                .contains("sub2 contacts: 2");
+    }
+
+    @ParameterizedTest
+    @EnumSource(SupportedGradleVersion.class)
     void test_groovy(SupportedGradleVersion gradleVersion) {
         final TestProjectBuilder builder = TestProjectBuilder.testProject(projectDir);
         builder.rootProject().plugins().id("com.netflix.nebula.contacts");
